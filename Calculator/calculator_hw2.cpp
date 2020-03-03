25b969b00b69538a34074d30aea7def0c1553424
Ruslan <45178528+RuslanLytvynov@users.noreply.github.com>
2020-03-03 17:57:36 +0100

    Add files via upload
    4-th try

--- Calculator/calculator_hw2.cpp
+++ Calculator/calculator_hw2.cpp
@@ -1,37 +1,36 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
 int main() {
-	double a,b,c;
-	char z;
-	cin >> z;
-	cin >> a >> b;
-	switch (z){
-		case '1':
-			c = a + b; 
-			cout << c;
-			break;			
-		case '2':		
-			c = a - b;
-			cout << c;
-			break;	
-		case '3':	
-			c = a * b;
-			cout << c;
-			break;	
-		case '4':		
-			c = a / b;
-			cout << c;
-			break;
-		case '5':		
-			c = sqrt(a) + sqrt(b);
-			cout << c;
-			break;		
-		default:
-			cout << "error";
-			break;
-	}
+	double a,b;
+	int calc;
+	cin >> calc;
+	if (calc == 5)
+	 {
+		switch (calc)
+		{
+		case 5:
+		cin >> a;
+		cout << pow(a,0.5);}
+	 }	
+	else {
+		cin >> a >> b;
+		switch (calc){
+			case 1:
+				cout << a+b;
+				break;
+			case 2:
+				cout << a-b;
+				break;
+			case 3:
+				cout << a*b;
+				break;
+			case 4:
+				cout << a/b;
+				break;
+			default:
+				cout << "error";
+				break;}
+		}
 }
-
